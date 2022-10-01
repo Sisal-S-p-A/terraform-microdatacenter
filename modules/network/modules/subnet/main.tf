3488ee1f3ab9b613ed64958b920ab9d4fce5eaea
Alberto Clemente <alberto.clemente@sisal.it>
2022-10-01 12:58:05 +0200

    fix subnet

--- modules/network/modules/subnet/main.tf
+++ modules/network/modules/subnet/main.tf
@@ -9,16 +9,16 @@ terraform {
 }
 
 locals {
-  name = var.name
-  cidr = var.cidr
+  name   = var.name
+  cidr   = var.cidr
   public = var.public
 }
 
 locals {
-  ctx = var.context
+  ctx         = var.context
   compartment = local.ctx.compartment
-  vcn = local.ctx.vcn
-  gateway = local.vcn.gateway
+  vcn         = local.ctx.vcn
+  gateway     = local.vcn.gateway
 }
 
 locals {
@@ -26,32 +26,32 @@ locals {
 }
 
 resource "oci_core_route_table" "route_table" {
-    compartment_id = local.compartment.id
-    vcn_id = local.vcn.id
+  compartment_id = local.compartment.id
+  vcn_id         = local.vcn.id
 
-    display_name = local.name
+  display_name = local.name
 
-    route_rules {
-        description = "Default route"
+  route_rules {
+    description = "Default route"
 
-        destination_type = "CIDR_BLOCK"
-        destination = "0.0.0.0/0"
+    destination_type = "CIDR_BLOCK"
+    destination      = "0.0.0.0/0"
 
-        network_entity_id = local.public ? local.gateway.internet.id : local.gateway.nat
+    network_entity_id = local.public ? local.gateway.internet.id : local.gateway.nat.id
+  }
+
+  dynamic "route_rules" {
+    for_each = local.public ? { service = local.gateway.service } : { }
+    content {
+      description = route_rules.value.serivice.service_name
+
+      destination_type = "SERVICE_CIDR_BLOCK"
+      destination      = route_rules.value.serivice.cidr_block
+
+      network_entity_id = local.gateway.service.id
     }
+  }
 
-    dynamic "route_rules" {
-        for_each = local.public ? local.gateway.service : {}
-        content {
-            description = route_rules.value.service_name
-
-            destination_type = "SERVICE_CIDR_BLOCK"
-            destination = route_rules.value.cidr_block
-
-            network_entity_id = local.gateway.service.id
-        }
-    }
-
-    freeform_tags = merge({
-    }, local.vcn.freeform_tags)
+  freeform_tags = merge({
+  }, local.vcn.freeform_tags)
 }
