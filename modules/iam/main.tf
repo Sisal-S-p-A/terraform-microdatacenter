eeba6422fe2e59fc1107cf79fa83d8026c555f10
Alberto Clemente <alberto.clemente@sisal.it>
2022-10-02 17:11:48 +0200

    fix policy statement

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -104,7 +104,7 @@ resource "oci_identity_policy" "admin_tenancy" {
       module.groups["admins"].group.name
     ),
 
-    format("Allow group %s to manage group in tenancy where target.group.name='%s'",
+    format("Allow group %s to manage groups in tenancy where target.group.name='%s'",
       module.groups["admins"].group.name,
       module.groups["operators"].group.name,
     ),
